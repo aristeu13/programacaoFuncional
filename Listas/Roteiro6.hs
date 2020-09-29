654ead8e24e2597390a7610d94e1fe01f7348e26
Aristeu <aristeuneto13@gmail.com>
2020-09-29 15:23:55 -0300

    quase terminando

--- Listas/Roteiro6.hs
+++ Listas/Roteiro6.hs
@@ -41,3 +41,66 @@ junta_silabasplural :: [String] -> String
 junta_silabasplural lst = foldr (++) "" lst
 
 -- 10
+bolha :: Ord a => [a] -> [a]
+bolha [] = []
+bolha lst = bolhaOrd lst (length lst)
+
+bolhaOrd :: Ord a => [a] -> Int -> [a]
+bolhaOrd lst 0 = lst
+bolhaOrd lst n = bolhaOrd (troca lst) (n-1)
+
+troca :: Ord a => [a] -> [a]
+troca [x] = [x]
+troca (x:y:zs)
+  | x > y     = y : troca (x:zs)
+  | otherwise = x : troca (y:zs)
+
+
+selecao :: Ord a => [a] -> [a]
+selecao [] = []
+selecao xs = [x] ++ selecao (remove x xs)
+  where x = minimo xs
+
+remove :: Ord a => a -> [a] -> [a]
+remove _ [] = []
+remove a (x:xs)
+  | a==x = xs
+  | otherwise = x:(remove a xs)
+
+minimo :: Ord a => [a] -> a
+minimo [] = undefined
+minimo [x] = x
+minimo (x:xs)
+  | x <= (minimo xs) = x
+  | otherwise = minimo xs
+
+
+insercao :: Ord a => [a] -> [a]
+insercao []     = []
+insercao (x:xs) = insereOrd x (insercao xs)
+
+insereOrd :: (Ord a) => a -> [a] -> [a]
+insereOrd x [] = [x]
+insereOrd x (y:ys)
+  | x <= y = (x:y:ys)
+  | otherwise = y:(insereOrd x ys)
+
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (s:xs) = quicksort [x|x <- xs, x < s] ++ [s] ++ quicksort [x|x <- xs, x >= s]
+
+-- 11
+-- bolha2 :: Ord a => [a] -> [a]
+-- bolha2 [] = []
+-- bolha2 lst = bolhaOrd2 lst (length lst)
+
+-- bolhaOrd2 :: Ord a => [a] -> Int -> [a]
+-- bolhaOrd2 lst 0 = lst
+-- bolhaOrd2 lst n = bolhaOrd2 (troca2 lst) (n-1)
+
+troca2 :: Ord a => [a] -> [a]
+troca2 [x] = [x]
+troca2 (x:y:zs)
+  | x > y     = y : troca2 (x:zs)
+  | otherwise = x : troca2 (y:zs)
